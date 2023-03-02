bdb4d9bf184fd0773a212135e4dd792eb6315d12
heynemann <bernardo.heynemann@coinbase.com>
2023-03-02 00:18:57 -0300

    chore: additional test cases

--- gocovparser/parsers_test.go
+++ gocovparser/parsers_test.go
@@ -3,6 +3,7 @@ package gocovparser_test
 //revive:disable:add-constant
 
 import (
+	"fmt"
 	"math"
 	"testing"
 
@@ -90,6 +91,26 @@ func TestPackageParser2(t *testing.T) {
 	require.Equal(t, 6919, getCov(t, got, "package", path))
 }
 
+func TestPackageParser3(t *testing.T) {
+	data1 := CoverageFixture3(t)
+
+	items, err := gocovparser.Parse(data1)
+	require.NoError(t, err)
+
+	// ACT
+	got, err := gocovparser.GroupCoverage(items, gocovparser.PackageParseGroup)
+
+	// ASSERT
+	require.NoError(t, err)
+
+	pkg := func(item string) string { return fmt.Sprintf("github.cbhq.net/engineering/docstore/%s", item) }
+
+	require.Equal(t, 7318, getCov(t, got, "package", pkg("bsonpb")))
+	require.Equal(t, 4545, getCov(t, got, "package", pkg("testhelpers")))
+	require.Equal(t, 1938, getCov(t, got, "package", pkg("sdk")))
+	require.Equal(t, 9222, getCov(t, got, "package", pkg("modules/mongo")))
+}
+
 func TestTotalParser(t *testing.T) {
 	data1 := CoverageFixture(t)
 
@@ -119,4 +140,36 @@ func TestTotalParser2(t *testing.T) {
 
 	coverage := got["total"]["total"]
 	require.Equal(t, float64(6919), math.Round(coverage*10000.0))
+}
+
+func TestTotalParser3(t *testing.T) {
+	data1 := CoverageFixture3(t)
+
+	items, err := gocovparser.Parse(data1)
+	require.NoError(t, err)
+
+	// ACT
+	got, err := gocovparser.GroupCoverage(items, gocovparser.TotalParseGroup)
+
+	// ASSERT
+	require.NoError(t, err)
+
+	coverage := got["total"]["total"]
+	require.Equal(t, float64(7497), math.Round(coverage*10000.0))
+}
+
+func TestTotalParser4(t *testing.T) {
+	data1 := CoverageFixture4(t)
+
+	items, err := gocovparser.Parse(data1)
+	require.NoError(t, err)
+
+	// ACT
+	got, err := gocovparser.GroupCoverage(items, gocovparser.TotalParseGroup)
+
+	// ASSERT
+	require.NoError(t, err)
+
+	coverage := got["total"]["total"]
+	require.Equal(t, float64(195), math.Round(coverage*10000.0))
 }
